06f056090b365d8b26a950e6b78dff7866971f81
João Victor Gomes <j.gomes0963@gmail.com>
2023-10-12 23:01:32 -0300

    Salvando modificação do Primo.java

--- Primo.java
+++ Primo.java
@@ -1,9 +1,34 @@
 //Dizer se o numero digitado e primo ou não!
 import java.util.Scanner; public class Primo{
     public static void main(String[] args) {
+        Scanner dd=new Scanner(System.in);
+       
+        //0==48 | 1==49 | 2==50 | 3==51 | 4==52 | 9==57
         System.out.println("Digite um numero: ");
-        String en=new Scanner(System.in).next();
+        String en=dd.next(); //12n4
+        String com="";
+        System.out.println("------------");
 
+        for(;;){ //trava de segurança: apenas numeros!!!
+            for(int i=0;en.length()>i;i++){
+                char tr[]=en.toCharArray();
+                if(tr[i]>=48 && tr[i]<=57){
+                    com+=tr[i];// 12
+                    System.out.println(com+"<-");
+                    break;
+                }if(tr[i]<48 && tr[i]>57){
+                    System.out.println("Digite apenas numero!");
+                    en=dd.next();//1234
+                    System.out.println("-----");
+                    break;
+                }
+            }            
+            if((com.equalsIgnoreCase(en))){break;}
+        } //fim da trave.
+        System.out.println(en+"<- oque saiu da trava");// Errado: 12n4 | Correto:1234
+        System.out.println(com+"<- valor de com");
+       
+      
         int i,nu,nr,cont;
         nu= Integer.parseInt(en);
         nr=0;
